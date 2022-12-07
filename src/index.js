62f4a68752ac4f7b2d093ac9bc6cb93d1bd8ea90
Viktar Zanimonets <vzanimonets@gmail.com>
2022-12-08 01:42:33 +0300

    base index.js

---
+++ src/index.js
@@ -0,0 +1,70 @@
+import * as os from 'os'
+
+const homedir = os.homedir()
+
+process.chdir(homedir)
+
+const parseArgs = arg =>
+	process.argv
+		.slice(2)
+		.map(str => {
+			if (str.includes(arg)) {
+				return str.slice(arg.length + 1)
+			}
+		})
+		.join('')
+
+const checkInput = async cmd => {
+	switch (cmd) {
+		case 'up':
+			return
+		case 'cd':
+			return
+		case 'ls':
+			return
+		case 'cat':
+			return
+		case 'add':
+			return
+		case 'rn':
+			return
+		case 'cp':
+			return
+		case 'mv':
+			return
+		case 'rm':
+			return
+		case 'os':
+			return
+		case 'hash':
+			return
+		case 'compress':
+			return
+		case 'decompress':
+			return
+		default:
+			console.log('smth where wrong!')
+	}
+	console.log(`You are currently in ${process.cwd()}`)
+}
+
+async function init() {
+	const argVal = parseArgs('--username')
+	const userName = argVal ? argVal : 'mr Someone'
+
+	process.stdout.write(`Welcome to the File Manager, ${userName}!\n`)
+
+	process.stdin.on('data', function (data) {
+		checkInput(data.toString().trim())
+	})
+
+	process.on('exit', data => {
+		process.exit(0)
+	})
+
+	process.on('SIGINT', () => {
+		console.log(`Thank you for using File Manager, ${userName}, goodbye!`)
+	})
+}
+
+await init()
