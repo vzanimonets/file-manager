87c68f60697a32eb1baef62031411eba66f4e894
Viktar Zanimonets <vzanimonets@gmail.com>
2022-12-19 01:10:27 +0300

    feat: add new commands and functions for works with arguments

--- src/index.js
+++ src/index.js
@@ -1,8 +1,21 @@
-import * as os from 'os'
-
-import { cd, ls, up } from './commands/index.js'
+import os from 'os'
 
 import * as constants from './constants/index.js'
+import {
+	add,
+	cat,
+	cd,
+	compress,
+	cp,
+	decompress,
+	hash,
+	ls,
+	mv,
+	os as _os,
+	rm,
+	rn,
+	up
+} from './commands/index.js'
 
 const homedir = os.homedir()
 
@@ -18,41 +31,88 @@ const parseArgs = arg =>
 		})
 		.join('')
 
-const checkInput = async cmd => {
-	switch (cmd) {
-		case 'up':
-			await up()
-			break
-		case 'cd':
-			await cd()
-			break
-		case 'ls':
-			await ls()
-			break
-		case 'cat':
-			return
-		case 'add':
-			return
-		case 'rn':
-			return
-		case 'cp':
-			return
-		case 'mv':
-			return
-		case 'rm':
-			return
-		case 'os':
-			return
-		case 'hash':
-			return
-		case 'compress':
-			return
-		case 'decompress':
-			return
-		default:
-			console.log(constants.OPERATION_ERROR_MESSAGE)
+const checkInput = async (cmd, args) => {
+	//TODO: need to refactor
+
+	const [arg1, arg2] = args
+
+	try {
+		switch (cmd) {
+			case 'up':
+				await up()
+				break
+			case 'ls':
+				await ls()
+				break
+			case 'cd':
+				await cd(arg1)
+				break
+			case 'cat':
+				cat(arg1)
+				return
+			case 'add':
+				await add(arg1)
+				return
+			case 'rn':
+				await rn(arg1, arg2)
+				return
+			case 'cp':
+				await cp(arg1, arg2)
+				return
+			case 'mv':
+				await mv(arg1, arg2)
+				return
+			case 'rm':
+				await rm(arg1)
+				return
+			case 'os':
+				_os(arg1)
+				return
+			case 'hash':
+				await hash(arg1)
+				return
+			case 'compress':
+				await compress(arg1, arg2)
+				return
+			case 'decompress':
+				await decompress(arg1, arg2)
+				break
+			default:
+				console.log(constants.INPUT_ERROR_MESSAGE)
+		}
+	} catch (e) {
+		console.log(constants.OPERATION_ERROR_MESSAGE)
 	}
 	console.log(`You are currently in ${process.cwd()}`)
+}
+
+const parseParameters = str => {
+	//TODO: need to refactor
+	let arg1
+	let arg2
+	let endArg1Idx
+	if (str.startsWith('"') || str.startsWith("'")) {
+		const quotes = str[0]
+		const firstQuotesIdx = str.indexOf(quotes)
+		const secondQuotesIdx = str.indexOf(quotes, firstQuotesIdx + 1)
+
+		arg1 = str.slice(firstQuotesIdx + 1, secondQuotesIdx)
+		endArg1Idx = secondQuotesIdx + 1
+	} else {
+		arg1 = str.split(' ', 1).toString()
+		endArg1Idx = arg1.length
+	}
+	const str2 = str.slice(endArg1Idx).trim()
+	if (str2.startsWith('"') || str2.startsWith("'")) {
+		const quotes = str2[0]
+		const firstQuotesIdx = str2.indexOf(quotes)
+		const secondQuotesIdx = str2.indexOf(quotes, firstQuotesIdx + 1)
+
+		arg2 = str2.slice(firstQuotesIdx + 1, secondQuotesIdx)
+	} else {
+		arg2 = str2
+	}
+	return [arg1, arg2]
 }
 
 async function init() {
@@ -60,18 +120,31 @@ async function init() {
 	const userName = argVal ? argVal : 'mr Someone'
 
 	process.stdout.write(`Welcome to the File Manager, ${userName}!\n`)
+	process.stdout.write(`You are currently in ${process.cwd()}\n`)
 
 	process.stdin.on('data', data => {
 		if (data.toString().includes('.exit')) process.exit(0)
-		checkInput(data.toString().trim())
+
+		const inputStr = data.toString().trim()
+		if (inputStr.includes('.exit')) process.exit(0)
+
+		const cmd = inputStr.split(' ', 1).toString()
+		const argsStr = inputStr.split(' ').slice(1).join(' ').trim()
+		const args = parseParameters(argsStr)
+
+		checkInput(cmd, args)
 	})
 
 	process.on('exit', data => {
+		console.log(`Thank you for using File Manager, ${userName}, goodbye!`)
 		process.exit(0)
 	})
 
 	process.on('SIGINT', () => {
-		process.stdout.write(`Thank you for using File Manager, ${userName}, goodbye!`)
+		process.stdout.write(
+			`Thank you for using File Manager, ${userName}, goodbye!`
+		)
 	})
 }
+
 await init()
