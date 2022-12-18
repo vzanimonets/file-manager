b7e007c16ea7cf82308ef92a03894f524893eb2e
Viktar Zanimonets <vzanimonets@gmail.com>
2022-12-19 00:23:49 +0300

    refactor: code refactor for function up, that navigate to the parent directory

--- src/commands/up.js
+++ src/commands/up.js
@@ -1,19 +1,10 @@
 import { chdir } from 'node:process'
-import * as path from 'path'
-
-import { FS_ERROR_MESSAGE } from '../constants/index.js'
-
-//TODO: move this function from this file
-const getRootDir = () => path.parse(process.cwd()).root
+import path from 'path'
 
 const up = async () => {
-	try {
-		const root = getRootDir()
-		if (process.cwd() !== root) {
-			chdir('..')
-		}
-	} catch (error) {
-		throw new Error(FS_ERROR_MESSAGE)
+	const root = path.parse(process.cwd()).root
+	if (process.cwd() !== root) {
+		chdir('..')
 	}
 }
 export { up }
