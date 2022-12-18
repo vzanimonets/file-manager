93a8cf457a8a8c43951349af106e12d47bb23eba
Viktar Zanimonets <vzanimonets@gmail.com>
2022-12-19 01:04:59 +0300

    refactor: add imports for the new commands

--- src/commands/index.js
+++ src/commands/index.js
@@ -1,5 +1,15 @@
 import { ls } from './ls.js'
 import { up } from './up.js'
 import { cd } from './cd.js'
+import { cat } from './cat.js'
+import { rn } from './rn.js'
+import { add } from './add.js'
+import { cp } from './cp.js'
+import { mv } from './mv.js'
+import { rm } from './rm.js'
+import { os } from './os.js'
+import { hash } from './hash.js'
+import { compress } from './compress.js'
+import { decompress } from './decompress.js'
 
-export { ls, up, cd }
+export { ls, up, cd, cat, rn, add, cp, mv, rm, os, hash, compress, decompress }
